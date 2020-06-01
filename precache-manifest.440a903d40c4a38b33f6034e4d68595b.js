11392298db35178a04255091d47519ca025b5ecc
Oscar Rangel <r4ngel93@debian-DELL>
2020-05-31 21:50:39 -0500

    Updates

---
+++ precache-manifest.440a903d40c4a38b33f6034e4d68595b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "638368f882e399ffdcd6adb6f8d03975",
+    "url": "/r4ng3l/index.html"
+  },
+  {
+    "revision": "b1800a650e74a9187ec5",
+    "url": "/r4ng3l/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "9dc0481c4aa6c963f1cf",
+    "url": "/r4ng3l/static/css/main.1dde9f19.chunk.css"
+  },
+  {
+    "revision": "b1800a650e74a9187ec5",
+    "url": "/r4ng3l/static/js/2.ee2732d8.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/r4ng3l/static/js/2.ee2732d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dc0481c4aa6c963f1cf",
+    "url": "/r4ng3l/static/js/main.af23e99f.chunk.js"
+  },
+  {
+    "revision": "3cffc4c3e0a8cc39eda9",
+    "url": "/r4ng3l/static/js/runtime-main.a22239f4.js"
+  }
+]);
